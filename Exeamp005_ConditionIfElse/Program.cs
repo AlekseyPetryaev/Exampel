7c7109c13eb63ad7e0f36802ce68b6364efcd355
Aleksey <petraevaleksej7@mail.com>
2023-01-13 13:26:05 +0300

    add Exeamp 005 Condition If Else

---
+++ Exeamp005_ConditionIfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") // как бы мы не записали слова с большой или с маленькой буквы теперь
+//                                   оно писаться как мы зададим ниже 
+//if(username == "Маша")
+{
+    Console.WriteLine("Ура, это же МАША");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+} 
+
