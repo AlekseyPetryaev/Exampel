ea8fc7d2ab63aecc8ec0c626f8f9fe2edffa4645
Aleksey <petraevaleksej7@mail.com>
2023-01-13 13:48:53 +0300

    add Exeamp 004 Div

---
+++ Exeamp004_Div/Program.cs
@@ -0,0 +1,12 @@
+﻿//int numberA = 10;
+//int numberB = 5;
+//Console.WriteLine(numberA / numberB);
+//2
+//int numberC = 12;
+//int numberB = 5;
+//Console.WriteLine(numberC / numberB);
+//2
+Double numberC = 12;
+Double numberB = 5;
+Console.WriteLine(numberC / numberB);
+//2,4
