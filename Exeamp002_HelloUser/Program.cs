3a1f081f19ebc5fb58a396c02b38088e6b7ba173
AlekseyPetryaev <petraevaleksej7@gmail.com>
2023-01-14 10:33:17 +0300

    add Exeamp 002 Hello User

--- Exeamp002_HelloUser/Program.cs
+++ Exeamp002_HelloUser/Program.cs
@@ -1,4 +1,23 @@
-﻿Console.Write("Введите ваше имя ");
-String username = Console.ReadLine( );
+﻿//Через Console мы обращаемся к терминалу
+/* 1
+//этой командой создаем в терминале страку для ввода слов,цифр и т.,д......
+string username = Console.ReadLine(); //в строка где надо вводить имя
+Console.WriteLine("Hello, "); //здесь задаем, что ответит ("Привет, ")
+Console.WriteLine(username); // дублирует заданное нами имя
+*/
+/* 
+//Console.WriteLine - вводит в терминал в столбик
+//будет отображать в терминал  ("то что тут укажим: ")
+Console.WriteLine("Введите ваше имя: "); //чтобы видеть куда вводить 
+string username = Console.ReadLine(); //строка где надо вводить имя в терминале
+Console.WriteLine("Hello, "); //здесь задаем, что ответит
+Console.WriteLine(username); // дублирует заданное нами имя
+*/
+/*
+//Console.Write - вводит в терминал в встрочку
+//будет отображать в терминал  ("то что тут укажим: ")
+Console.Write("Введите ваше имя: "); //чтобы видеть куда вводить
+string username = Console.ReadLine(); //строка где надо вводить имя в терминале
 Console.Write("Привет, ");
-Console.WriteLine(username);
+Console.WriteLine(username);// дублирует заданное нами имя
+*/
