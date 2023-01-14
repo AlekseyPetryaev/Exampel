250a0a7a430586a37bd01152246bee3f16a12d69
AlekseyPetryaev <petraevaleksej7@gmail.com>
2023-01-14 11:02:09 +0300

    add Exeamp 001 Hello Console

--- Exeamp001_HelloConsole/Program.cs
+++ Exeamp001_HelloConsole/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine(" 1 Hello, World ");
+﻿// команда для ввода в терминал
+Console.WriteLine("1 Hello, Worold ");
+// 1 Hello, World 
+Console.WriteLine("2 Hello, World ");
+Console.WriteLine("3 Hello, World ");
+// 1 Hello, World 
+// 1 Hello, World 
+Console.Write(" 4 Hello, World ");
+Console.WriteLine(" 5 Hello, World ");
+//Terminal 1 Hello, World  1 Hello, World
