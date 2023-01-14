9deb5c649b0ee326b0eb8eb39bd5665ad19db820
Aleksey <petraevaleksej7@mail.com>
2023-01-14 19:09:21 +0300

    add Exeamp 003 Sum

--- Exeamp003_Sum/Program.cs
+++ Exeamp003_Sum/Program.cs
@@ -1,14 +1,49 @@
-﻿// Сумма двух чисел
-//int numberA = 31;
-//int numberB = 5;
-//int result = numberA + numberB;
-//Console.WriteLine(result);
+﻿// Сумма двух чисел, первый способ
+/*
+int numberA = 3; // Вводим число
+int numberB = 5; // Ввщдим число
+Console.WriteLine(numberA + numberB); //вывод суммы
+*/
+/*
+// Сумма двух чисел, второй способ с использованием переменной -(result)
+int numberC = 333;
+int numberD = 444;
+int result = numberC + numberD; //дополнительная переменная в которой будет храниться сумма
+Console.WriteLine(result); //выводит сумму 
+*/
+// Разность
+/*
+int numberE = 332;
+int numberF = 331;
+Console.WriteLine(numberE - numberF);
+*/
+/*
+// Разность, второй способ с использованием переменной -(result)
+int numberG = 895;
+int numberH = 589;
+int result = (numberG - numberH);
+Console.WriteLine(result);
+*/
+/*
+// Умножение 
+int numberI = 53;
+int numberJ = 74;
+Console.WriteLine(numberI * numberJ);
+*/
+/*
+//Деление 
+int numberK = 55;
+int numberL = 5;
+Console.WriteLine(numberK / numberL);
+*/
 
-//int numberA = new Random().Next(1, 10); // 1 2 3 4 ...9
-//int numberB = new Random().Next(1, 10);
-//int result = numberA + numberB;
-//Console.WriteLine(result);
+/*
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ...9
+int numberB = new Random().Next(1, 10);
+int result = numberA + numberB;
+Console.WriteLine(result);
 //9
+/*
 int numberA = new Random().Next(1, 10); // 1 2 3 4 ...9
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
@@ -18,4 +53,5 @@ Console.WriteLine(result);
 //6
 //3
 //9
+*/
 
