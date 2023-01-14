c1627fd2bdd3c4624ee2dd0af30d811ba1b17b3b
Aleksey <petraevaleksej7@mail.com>
2023-01-14 20:50:52 +0300

    add Exeamp 003 Sum

--- Exeamp003_Sum/Program.cs
+++ Exeamp003_Sum/Program.cs
@@ -36,22 +36,24 @@ int numberK = 55;
 int numberL = 5;
 Console.WriteLine(numberK / numberL);
 */
-
 /*
+//Вводим число = случайное целое число 
 int numberA = new Random().Next(1, 10); // 1 2 3 4 ...9
 int numberB = new Random().Next(1, 10);
 int result = numberA + numberB;
 Console.WriteLine(result);
-//9
-/*
-int numberA = new Random().Next(1, 10); // 1 2 3 4 ...9
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.WriteLine(result);
-//6
-//3
 //9
 */
+
+//Вводим число = случайное целое число // указываем диапазон .Next(1, 10)
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ...9
+Console.WriteLine(numberA); //будет показывать в терминеале случайное число
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB); //будет показывать в терминеале случайное число
+int result = numberA + numberB; //дополнительная переменная в которой будет храниться сумма
+Console.WriteLine(result); // вывод суммы через переменную 
+//6 петвое случайное число 
+//3 второе случайное число
+//9 сумма первого и второго случайного числа
+
 
