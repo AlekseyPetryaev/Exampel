0f62c8b684f96e0b0e55cbbbd368976e6ce21a32
Aleksey <petraevaleksej7@mail.com>
2023-01-14 19:27:33 +0300

    add Exeamp 004 Div

--- Exeamp004_Div/Program.cs
+++ Exeamp004_Div/Program.cs
@@ -1,12 +1,22 @@
-﻿//int numberA = 10;
-//int numberB = 5;
-//Console.WriteLine(numberA / numberB);
+﻿/*
+// Деление целых чисел (int)
+int numberA = 10;
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
 //2
-//int numberC = 12;
-//int numberB = 5;
-//Console.WriteLine(numberC / numberB);
+*/
+/*
+//Деление целых чисел (int)
+int numberC = 12;
+int numberB = 5;
+Console.WriteLine(numberC / numberB);
 //2
+*/
+/*
+//Деление вещественных чисел (double)
 Double numberC = 12;
 Double numberB = 5;
 Console.WriteLine(numberC / numberB);
 //2,4
+*/
+
