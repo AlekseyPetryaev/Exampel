a34c595208bdc9e839e7135dacbb52ee3661c29d
Aleksey <petraevaleksej7@mail.com>
2023-01-31 11:12:00 +0300

    add Sem 4 DZ

---
+++ Sem4_DZ/Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа 
+//(А и В) и возводит число А в натуральную степень В.
+// Реализовать функцию возведения в степень самостоятельно!
+// 3 , 5 -> 243(3 в 5)
+// 2, 4 -> 16 
+Console.Clear();
+Console.Write("Введите число 1): ");
+int num = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число 2): ");
+int numA = int.Parse(Console.ReadLine()!);
+int num3 = num;
+void Method1(int num, int numA)
+{
+    int num3 = num;
+    for(int i = 1 ;i < numA;i++)
+    {
+        num *= num3;   
+    }
+    Console.Write($"{num} ");
+}
+Method1(num ,numA);
+
---
+++ Sem4_DZ/Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+
+Console.Clear();
+Console.Write("Введите число : ");
+int num = int.Parse(Console.ReadLine()!);
+int sum = 0;
+int number = 0;
+void Met(string[] args)
+{   
+    while (num > 0)
+    {
+        number = num % 10;
+        sum = sum + number;
+        num = num /10;
+    }
+    Console.Write($"{sum} ");
+}
+Met(args);
+Console.WriteLine("");
+
+
+
